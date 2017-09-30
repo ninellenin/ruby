ac62fa6c00d88bffaa28e38b61c8ec17472a2b63
Sazonova Yuliya <yulia-saz3@yandex.ru>
2017-09-30 11:17:13 +0700

    Added methods

--- lab2.rb
+++ lab2.rb
@@ -5,12 +5,36 @@ class ParallelArray < Array
         result = i * (size / threads_num) + ((size % threads_num > i)? i: (size % threads_num))
     end
     def parallel(threads_num, &handler)
+        result = {}
         (0..threads_num - 1).map{|i| 
-            yield self[get_bound(i, threads_num) .. get_bound(i + 1, threads_num) - 1]
+            new_thread = java.lang.Thread.new {
+                result[i] = yield self[get_bound(i, threads_num) .. get_bound(i + 1, threads_num) - 1]
+            }
+            new_thread.run
+            [i, new_thread]
+        }.map{|thread| 
+            thread[1].join 
+            result[thread[0]]}
+    end
+    def parallel_any(threads_num=2, &handler)
+        result = parallel(threads_num) {
+            |sub_array| sub_array.any?(&handler)
         }
+        result.any?
+    end
+    def parallel_all(threads_num=2,&handler)
+        result = parallel(threads_num) {
+            |sub_array| sub_array.all?(&handler)
+        }
+        result.all?
     end
     def parallel_map(threads_num=2, &handler)
-        parallel(threads_num, &handler)
+        result = parallel(threads_num){|sub_array| sub_array.map(&handler)}
+        result.flatten(1)
+    end
+    def parallel_select(threads_num=2, &handler)
+        result = parallel(threads_num) {|sub_array| sub_array.select(&handler)}
+        result.flatten(1)
     end
 end
 
@@ -35,8 +59,15 @@ def test_get_bound_method()
     }.reduce{|result, test| (result and test) }
 end
 
-array = ParallelArray.new([1, 2, 3, 4, 5])
+def test()
+    puts("Test get_bound_method: " + test_get_bound_method.to_s)
+    array = ParallelArray.new([1, 2, 3, 4, 5])
+    puts("Test parallel map: "+ (array.parallel_map(2){|i| i + 1} == [2, 3, 4, 5, 6]).to_s)
+    puts("Test parallel any: " + (array.parallel_any(3){|i| i > 3} == true and 
+        array.parallel_any(3){|i| i < 1} == false).to_s)
+    puts("Test parallel all: " + (array.parallel_all(3){|i| i > 3} == false and 
+        array.parallel_all(3){|i| i > 0} == true).to_s)
+    puts("Test parallel select: " + (array.parallel_select(3){|i| i % 2 == 1 } == [1, 3, 5]).to_s)
+end
 
-puts("Test get_bound_method: " + test_get_bound_method.to_s)
-#puts(array.map{|i| i + 1})
-array.parallel(2){|i| puts(i.to_s)}
+test
